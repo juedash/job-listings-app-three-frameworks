39e874d8bf73e431853a3771195ccd774e20fb9b
juedash <juedashherifi@hotmail.com>
2025-02-04 16:44:32 +0100

    react project added

---
+++ react-job-portal/src/App.jsx
@@ -0,0 +1,62 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import HomePage from "./pages/HomePage.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+import JobsPage from "./pages/JobsPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import JobPage, {jobLoader} from "./pages/JobPage.jsx";
+import AddJobPage from "./pages/AddJobPage.jsx";
+import EditJobPage from "./pages/EditJobPage.jsx";
+
+// Add New Job
+const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newJob),
+    });
+    return;
+};
+
+// Delete Job
+const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+        method: 'DELETE',
+    });
+    return;
+};
+
+// Update Job
+const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(job),
+    });
+    return;
+};
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout/>} >
+            <Route index element={<HomePage />} />
+            <Route path="/jobs" element={<JobsPage/>}/>
+            <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
+            <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>} />
+            <Route
+                path='/jobs/edit-job/:id'
+                element={<EditJobPage updateJobSubmit={updateJob} />}
+                loader={jobLoader}
+            />
+            <Route path="*" element={<NotFoundPage/>}/>
+        </Route>
+));
+const App = () => {
+    return <RouterProvider router={router}/>
+}
+
+export default App;
---
+++ react-job-portal/src/pages/JobsPage.jsx
@@ -0,0 +1,9 @@
+import JobListings from "../components/JobListings.jsx";
+
+const JobsPage = () => {
+    return (
+        <JobListings/>
+        )
+}
+
+export default JobsPage;
